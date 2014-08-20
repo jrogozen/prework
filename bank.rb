f6315a8ee0741b8fadc20f156afe3458c9b93baf
jon rogozen <jon.rogozen@gmail.com>
2014-08-20 10:39:39 -0700

    add loan methods

--- bank.rb
+++ bank.rb
@@ -1,6 +1,7 @@
 class BankAccount
 	def initialize
 		@amount_available = 0
+		@loan_balance = 0
 	end
 	def deposit_money amount
 		@amount_available += amount
@@ -12,4 +13,29 @@ class BankAccount
 		puts @amount_available
 		return @amount_available
 	end
+
+	def take_loan amount
+		@loan_balance += amount
+		@amount_available += amount
+	end
+	def repay_loan amount
+		@loan_balance -= amount
+	end
+	def accrue_interest
+		interest = @loan_balance * 0.06
+		@loan_balance += interest
+	end
+	def get_outstanding_loan
+		puts @loan_balance
+		return @loan_balance
+	end
 end
+
+boa = BankAccount.new
+boa.deposit_money(500)
+boa.withdraw_money(100)
+boa.get_balance
+boa.take_loan(500)
+boa.repay_loan(400)
+boa.accrue_interest
+boa.get_outstanding_loan
