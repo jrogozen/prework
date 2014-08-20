163d34f62f6f31adcccf336da987f38b1ef5f880
jon rogozen <jon.rogozen@gmail.com>
2014-08-20 15:27:50 -0700

    add book flight method

--- airplanes.rb
+++ airplanes.rb
@@ -1,60 +1,61 @@
 class Airplane
 	attr_accessor :city
 
-	@@city_to_airplane_hash = {}
+	@@city_to_airplane_hash = Hash.new([])
+	@@flight_plans = {}
+	#{airplane => city}
 
 	def self.list_airplanes
 		p @@city_to_airplane_hash
-	end
-	def self.can_i_fly? (city1, city2)
-		if @@city_to_airplane_hash.has_key?(city1) && !@@city_to_airplane_hash.has_key?(city2) 
-			puts "You can fly from #{city1} to #{city2}."
-		else
-			puts "Flight not available."
-		end
 	end
 
 	def initialize (city, capacity)
 		@city = city
 		@capacity = capacity
+		@people_on_plane = 0
 
-		if @@city_to_airplane_hash.has_key?(@city)
-			puts "There is already an airplane in #{city}. Please type a new city."
-			until !@@city_to_airplane_hash.has_key?(@city)
-				print ">>"
-				@city = gets.chomp
-			end
-			@@city_to_airplane_hash[@city] = self
-			puts "New airplane added to #{@city}."
-		else
-			@@city_to_airplane_hash[@city] = self
-			puts "New airplane added to #{@city}."
-		end
+		@@city_to_airplane_hash[@city] += [self]
 	end
 
 	def fly_to_city (city)
-		if @@city_to_airplane_hash.has_key?(city)
-			puts "Sorry, there is already an airplane in #{city}."
-		else
-			# delete current airplane
-			@@city_to_airplane_hash.delete(@city)
-			puts "Airplane has left #{@city}."
+		# delete current airplane
+		@@city_to_airplane_hash[@city].delete(self)
+		puts "Airplane has left #{@city}."
 
-			# add to new city
-			@@city_to_airplane_hash[city] = self
-			self.city = city
-			puts "Airplane has successfully arrived at #{city}."
-		end
+		# add to new city
+		@@city_to_airplane_hash[city] += [self]
+		self.city = city
+		puts "Airplane has successfully arrived at #{city}."
 	end
 
+	def book_flight (city)
+		# is flight already going somewhere else?
+		if @@flight_plans[self].nil? || @@flight_plans[self] == city
+			@@flight_plans[self] = city
+
+			# update plane count
+			if @people_on_plane + 1 <= @capacity
+				@people_on_plane += 1
+				puts "Flight to #{city} successfully booked."
+			else
+				puts "Sorry, flight is at maximum capacity."
+			end
+		else
+			puts "Sorry, flight is scheduled to go to a different city."
+		end
+
+	end
 end
 
-a = Airplane.new("SF", 100)
-b = Airplane.new("SF", 200)
-c = Airplane.new("LA", 100)
+a = Airplane.new("SF", 2)
+b = Airplane.new("SF", 2)
+c = Airplane.new("LA", 2)
 
 a.fly_to_city("LA")
 a.fly_to_city("MIN")
+a.fly_to_city("SF")
 Airplane.list_airplanes
-Airplane.can_i_fly?("SF", "LA")
-Airplane.can_i_fly?("MIN", "Houston")
+a.book_flight("LA")
+a.book_flight("SF")
+a.book_flight("LA")
+a.book_flight("LA")
